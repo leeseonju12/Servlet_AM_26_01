b88e81058271c44a5be2bf55ea93d547929296b4
leeseonju12 <seonju220@gmail.com>
2026-01-20 11:13:26 +0900

    Service / DAO 구조 추가 - List에만 적용

---
+++ src/main/java/com/KoreaIT/java/AM_jsp/service/ArticleService.java
@@ -0,0 +1,28 @@
+package com.KoreaIT.java.AM_jsp.service;
+
+import java.sql.Connection;
+import java.util.List;
+import java.util.Map;
+
+import com.KoreaIT.java.AM_jsp.dao.ArticleDao;
+import com.KoreaIT.java.AM_jsp.util.DBUtil;
+import com.KoreaIT.java.AM_jsp.util.SecSql;
+
+public class ArticleService {
+
+	private Connection conn;
+	private ArticleDao articleDao;
+
+	public ArticleService(Connection conn) {
+		this.conn = conn;
+		this.articleDao = new ArticleDao(conn);
+	}
+
+	public int getTotalCnt() {
+		return articleDao.getTotalCnt();
+	}
+
+	public List<Map<String, Object>> getForPrintArticles(int limitFrom, int itemsInAPage) {
+		return articleDao.getForPrintArticles(limitFrom, itemsInAPage);
+	}
+}
